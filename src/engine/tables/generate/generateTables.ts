8c9ac03239e91455e4ee9a1378c409e67ed37b09
drumevp <peter.drumev98@gmail.com>
2026-06-13 19:37:56 +0300

    Update most lookup tables to 32bit hi lo tables

--- src/engine/tables/generate/generateTables.ts
+++ src/engine/tables/generate/generateTables.ts
@@ -25,6 +25,14 @@
  *           1           b1        10
  *           2           c1        100
  *           3           d1        1000
+ *
+ *
+ * 32 BIT REFACTOR
+ * Rather than updating all the functions to calculate Lo and Hi,
+ * I will split the resulting bb to Lo and Hi and store the Lo and Hi tables
+ *
+ * Any 2D array, I will flatten to a 1D array and perform access another way.
+ * The performance of the bigint engine was lowered by the performance cost of accessing 2D arrays, it adds overhead.
  */
 
 import generateBetweenSquaresTable from "./generateBetweenSquaresTable";
@@ -55,13 +63,24 @@ import { rookShift } from "./rook/shift";
 import { Bitboard } from "../../types/bitboard";
 import { NUMBER_OF_PIECE_CATEGORIES } from "../../constants/piece";
 import { random64bit } from "../../helpers/main";
+import { LOWER_32_BITS_MASK } from "../../constants/mask";
+
+const splitBitboard = (bitboard: Bitboard): { lo: number; hi: number } => {
+  return {
+    lo: Number(bitboard & LOWER_32_BITS_MASK) >>> 0,
+    hi: Number((bitboard >> 32n) & LOWER_32_BITS_MASK) >>> 0,
+  };
+};
 
 // King
-export const kingLookupTable: Bitboard[] = [];
 export const kingMovementFns = [N, E, W, S, NE, NW, SE, SW];
 
+const kingLookupTableLo: Uint32Array = new Uint32Array(64);
+const kingLookupTableHi: Uint32Array = new Uint32Array(64);
+
 // Knight
-export const knightLookupTable: Bitboard[] = [];
+const knightLookupTableLo: Uint32Array = new Uint32Array(64);
+const knightLookupTableHi: Uint32Array = new Uint32Array(64);
 export const knightMovementFns = [
   moveNNW,
   moveNNE,
@@ -74,11 +93,13 @@ export const knightMovementFns = [
 ];
 
 // White Pawn
-export const whitePawnAttackTable: Bitboard[] = [];
+const whitePawnLookupTableLo: Uint32Array = new Uint32Array(64);
+const whitePawnLookupTableHi: Uint32Array = new Uint32Array(64);
 export const whitePawnMovementFns = [NW, NE];
 
 // Black Pawn
-export const blackPawnAttackTable: Bitboard[] = [];
+const blackPawnLookupTableLo: Uint32Array = new Uint32Array(64);
+const blackPawnLookupTableHi: Uint32Array = new Uint32Array(64);
 export const blackPawnMovementFns = [SW, SE];
 
 export const shift = (
@@ -93,16 +114,35 @@ export const shift = (
 let currentPositionBitboard: Bitboard = 0x1n;
 
 for (let i = 0; i < 64; i++) {
-  kingLookupTable[i] = shift(currentPositionBitboard, kingMovementFns);
-  knightLookupTable[i] = shift(currentPositionBitboard, knightMovementFns);
-  whitePawnAttackTable[i] = shift(
+  const kingBitboard = shift(currentPositionBitboard, kingMovementFns);
+  const { lo: kingBitboardLo, hi: kingBitboardHi } =
+    splitBitboard(kingBitboard);
+  kingLookupTableLo[i] = kingBitboardLo;
+  kingLookupTableHi[i] = kingBitboardHi;
+
+  const knightBitboard = shift(currentPositionBitboard, knightMovementFns);
+  const { lo: knightBitboardLo, hi: knightBitboardHi } =
+    splitBitboard(knightBitboard);
+  knightLookupTableLo[i] = knightBitboardLo;
+  knightLookupTableHi[i] = knightBitboardHi;
+
+  const whitePawnBitboard = shift(
     currentPositionBitboard,
     whitePawnMovementFns,
   );
-  blackPawnAttackTable[i] = shift(
+  const { lo: whitePawnBitboardLo, hi: whitePawnBitboardHi } =
+    splitBitboard(whitePawnBitboard);
+  whitePawnLookupTableLo[i] = whitePawnBitboardLo;
+  whitePawnLookupTableHi[i] = whitePawnBitboardHi;
+
+  const blackPawnBitboard = shift(
     currentPositionBitboard,
     blackPawnMovementFns,
   );
+  const { lo: blackPawnBitboardLo, hi: blackPawnBitboardHi } =
+    splitBitboard(blackPawnBitboard);
+  blackPawnLookupTableLo[i] = blackPawnBitboardLo;
+  blackPawnLookupTableHi[i] = blackPawnBitboardHi;
 
   currentPositionBitboard = currentPositionBitboard << 1n;
 }
@@ -168,19 +208,156 @@ for (let file = 0; file < 8; file++) {
   zobristEnPassantFileKeys[file] = random64bit();
 }
 
+/**
+ * Convert rook and bishop relevant blocker masks to Lo and Hi
+ * Rook and bishop magic numbers Lo and Hi
+ */
+const rookRelevantBlockerMaskLo: Uint32Array = new Uint32Array(64);
+const rookRelevantBlockerMaskHi: Uint32Array = new Uint32Array(64);
+
+const bishopRelevantBlockerMaskLo: Uint32Array = new Uint32Array(64);
+const bishopRelevantBlockerMaskHi: Uint32Array = new Uint32Array(64);
+
+// Magic numbers
+const rookMagicLo: Uint32Array = new Uint32Array(64);
+const rookMagicHi: Uint32Array = new Uint32Array(64);
+
+const bishopMagicLo: Uint32Array = new Uint32Array(64);
+const bishopMagicHi: Uint32Array = new Uint32Array(64);
+
+for (let i = 0; i < 64; i++) {
+  // Rook blocker mask
+  const rookBlockerMaskBitboard = rookRelevantBlockerMask[i];
+
+  const { lo: rookBlockerMaskBitboardLo, hi: rookBlockerMaskBitboardHi } =
+    splitBitboard(rookBlockerMaskBitboard);
+
+  rookRelevantBlockerMaskLo[i] = rookBlockerMaskBitboardLo;
+  rookRelevantBlockerMaskHi[i] = rookBlockerMaskBitboardHi;
+
+  // Bishop blocker mask
+  const bishopBlockerMaskBitboard = bishopRelevantBlockerMask[i];
+
+  const { lo: bishopBlockerMaskBitboardLo, hi: bishopBlockerMaskBitboardHi } =
+    splitBitboard(bishopBlockerMaskBitboard);
+
+  bishopRelevantBlockerMaskLo[i] = bishopBlockerMaskBitboardLo;
+  bishopRelevantBlockerMaskHi[i] = bishopBlockerMaskBitboardHi;
+
+  // Rook magic
+  const rookMagicBitboard = rookMagic[i];
+
+  const { lo: rookMagicBitboardLo, hi: rookMagicBitboardHi } =
+    splitBitboard(rookMagicBitboard);
+
+  rookMagicLo[i] = rookMagicBitboardLo;
+  rookMagicHi[i] = rookMagicBitboardHi;
+
+  // Bishop magic
+  const bishopMagicBitboard = bishopMagic[i];
+
+  const { lo: bishopMagicBitboardLo, hi: bishopMagicBitboardHi } =
+    splitBitboard(bishopMagicBitboard);
+
+  bishopMagicLo[i] = bishopMagicBitboardLo;
+  bishopMagicHi[i] = bishopMagicBitboardHi;
+}
+
+/**
+ * Rook & bishop magic attacks
+ *
+ * I am converting this to a 1D array to avoid the compute overhead from accessing the 2D array I earlier discovered.
+ * To be able to access it I need to know how many attacks there are for each square index, so I will store this in a
+ * separate table rookMagicAttackOffsets for ease of access later on.
+ *
+ *
+ */
+
+const rookMagicAttackOffsets: Uint32Array = new Uint32Array(64);
+
+let totalRookAttackEntries = 0;
+
+for (let square = 0; square < 64; square++) {
+  rookMagicAttackOffsets[square] = totalRookAttackEntries;
+  totalRookAttackEntries += rookMagicIndexedAttackTable[square].length;
+}
+
+const rookMagicAttacksLo: Uint32Array = new Uint32Array(totalRookAttackEntries);
+const rookMagicAttacksHi: Uint32Array = new Uint32Array(totalRookAttackEntries);
+
+for (let square = 0; square < 64; square++) {
+  const offset = rookMagicAttackOffsets[square];
+  const attacks = rookMagicIndexedAttackTable[square];
+
+  for (let attackIndex = 0; attackIndex < attacks.length; attackIndex++) {
+    const attackBitboard = attacks[attackIndex];
+
+    const { lo: attackBitboardLo, hi: attackBitboardHi } =
+      splitBitboard(attackBitboard);
+
+    rookMagicAttacksLo[offset + attackIndex] = attackBitboardLo;
+    rookMagicAttacksHi[offset + attackIndex] = attackBitboardHi;
+  }
+}
+
+const bishopMagicAttackOffsets: Uint32Array = new Uint32Array(64);
+
+let totalBishopAttackEntries = 0;
+
+for (let square = 0; square < 64; square++) {
+  bishopMagicAttackOffsets[square] = totalBishopAttackEntries;
+  totalBishopAttackEntries += bishopMagicIndexedAttackTable[square].length;
+}
+
+const bishopMagicAttacksLo: Uint32Array = new Uint32Array(
+  totalBishopAttackEntries,
+);
+const bishopMagicAttacksHi: Uint32Array = new Uint32Array(
+  totalBishopAttackEntries,
+);
+
+for (let square = 0; square < 64; square++) {
+  const offset = bishopMagicAttackOffsets[square];
+  const attacks = bishopMagicIndexedAttackTable[square];
+
+  for (let attackIndex = 0; attackIndex < attacks.length; attackIndex++) {
+    const attackBitboard = attacks[attackIndex];
+
+    const { lo: attackBitboardLo, hi: attackBitboardHi } =
+      splitBitboard(attackBitboard);
+
+    bishopMagicAttacksLo[offset + attackIndex] = attackBitboardLo;
+    bishopMagicAttacksHi[offset + attackIndex] = attackBitboardHi;
+  }
+}
+
 export default {
-  kingAttacks: kingLookupTable,
-  knightAttacks: knightLookupTable,
-  whitePawnAttacks: whitePawnAttackTable,
-  blackPawnAttacks: blackPawnAttackTable,
-  rookRelevantBlockerMasks: rookRelevantBlockerMask,
+  kingAttacksLo: kingLookupTableLo,
+  kingAttacksHi: kingLookupTableHi,
+  knightAttacksLo: knightLookupTableLo,
+  knightAttacksHi: knightLookupTableHi,
+  whitePawnAttacksLo: whitePawnLookupTableLo,
+  whitePawnAttacksHi: whitePawnLookupTableHi,
+  blackPawnAttacksLo: blackPawnLookupTableLo,
+  blackPawnAttacksHi: blackPawnLookupTableHi,
+  rookRelevantBlockerMasksLo: rookRelevantBlockerMaskLo,
+  rookRelevantBlockerMasksHi: rookRelevantBlockerMaskHi,
   rookShifts: rookShift,
-  rookMagicNumbers: rookMagic,
-  rookMagicAttacks: rookMagicIndexedAttackTable,
-  bishopRelevantBlockerMasks: bishopRelevantBlockerMask,
+  rookMagicNumbersLo: rookMagicLo,
+  rookMagicNumbersHi: rookMagicHi,
+  rookMagicAttackOffsets: rookMagicAttackOffsets,
+  rookMagicAttacksLo: rookMagicAttacksLo,
+  rookMagicAttacksHi: rookMagicAttacksHi,
+  bishopRelevantBlockerMasksLo: bishopRelevantBlockerMaskLo,
+  bishopRelevantBlockerMasksHi: bishopRelevantBlockerMaskHi,
   bishopShifts: bishopShift,
-  bishopMagicNumbers: bishopMagic,
-  bishopMagicAttacks: bishopMagicIndexedAttackTable,
+  bishopMagicNumbersLo: bishopMagicLo,
+  bishopMagicNumbersHi: bishopMagicHi,
+  bishopMagicAttackOffsets: bishopMagicAttackOffsets,
+  bishopMagicAttacksLo: bishopMagicAttacksLo,
+  bishopMagicAttacksHi: bishopMagicAttacksHi,
+
+  // TODO:
   squareBitboards,
   squareIndexByBitboard,
   betweenSquares,
--- src/engine/tables/generated/zobristBlackToMoveKey.ts
+++
@@ -1,3 +0,0 @@
-// Auto-generated. Do not edit manually.
-
-export const zobristBlackToMoveKey: bigint = 13690845936774407802n;
